37f149d529c13640b37069f5e611ba9510dccd52
KALAIPRIYAMpm <kalaipriyan7777@gmail.com>
2025-07-31 14:44:04 +0530

    feat:crud

--- src/App.java
+++ src/App.java
@@ -1,4 +1,3 @@
-
 import java.sql.*;
 
 public class App {
@@ -6,34 +5,46 @@ public class App {
         String url = "jdbc:mysql://localhost:3306/training";
         String username = "root";
         String password = "kalaipriyan0007"; 
-
+        // String dbname = "Newone";
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to MySQL!");
-            Statement name = conn.createStatement();
+            Statement stmt = conn.createStatement();
+            // String query = "CREATE DATABASE IF NOT EXISTS " + dbname;
+            // stmt.executeUpdate(query);
+ String createTable = "CREATE TABLE IF NOT EXISTS newone ("
+                    + "id INT PRIMARY KEY AUTO_INCREMENT, "
+                    + "name VARCHAR(100), "
+                    + "age INT, "
+                    + "mark INT, "
+                    + "course_id INT)";
+            stmt.executeUpdate(createTable);
+            System.out.println("Table created successfully");
             String InsertQuery = ("insert into detail values(?,?,?,?,?)");
-            PreparedStatement stmt = conn.prepareStatement(InsertQuery);
-            stmt.setInt(1, 108);  
-            stmt.setString(2, "Kalaipriyan");
-            stmt.setInt(3, 12);
-            stmt.setInt(4, 20);
-            stmt.setInt(5, 20);
-            int rowInserted = stmt.executeUpdate();
-            System.out.println("inserted"+rowInserted);
-            ResultSet res=name.executeQuery(" select * from detail");
-
+            PreparedStatement stmts = conn.prepareStatement(InsertQuery);
+            stmts.setInt(1,1991);
+            stmts.setString(2, "Moulee");
+            stmts.setInt(3, 20);
+            stmts.setInt(4, 90);
+            stmts.setInt(5,100);
+            int rowInserted = stmts.executeUpdate();
+            System.out.println("Inserted"+rowInserted);
+            ResultSet res = stmt.executeQuery("select * from detail");
             while(res.next()){
-                System.out.println(res.getInt("id")+" "+ res.getString("name")+" "+res.getInt("course_id")+" "+res.getInt("mark")+" "+res.getInt("age"));
+                System.out.println(res.getInt("id")+"  "+res.getString("name")+"  "+res.getInt("course_id")+"  "+res.getInt("mark")+"  "+res.getInt("age")+"  ");
             }
-
-            name.close();
+            // System.out.println("Database created or already exists.");\
             stmt.close();
+            stmts.close();
             conn.close();
-
+            res.close();
         } catch (SQLException e) {
-            System.out.println("Connection failed!");   
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found!");
             e.printStackTrace();
         }
     }
 }
-
